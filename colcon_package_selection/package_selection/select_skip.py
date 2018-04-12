95ffd4807744ab0b5475bcc56472d88db091b2d2
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-04-11 20:30:37 -0700

    rename whitelist/blacklist to select/skip

--- colcon_package_selection/package_selection/select_skip.py
+++ colcon_package_selection/package_selection/select_skip.py
@@ -8,9 +8,7 @@ from colcon_core.package_selection import PackageSelectionExtensionPoint
 from colcon_core.plugin_system import satisfies_version
 
 
-class WhitelistBlacklistPackageSelectionExtension(
-    PackageSelectionExtensionPoint
-):
+class SelectSkipPackageSelectionExtension(PackageSelectionExtensionPoint):
     """Select a set of packages based on a whitelist / blacklist."""
 
     def __init__(self):  # noqa: D107
@@ -20,66 +18,66 @@ class WhitelistBlacklistPackageSelectionExtension(
 
     def add_arguments(self, *, parser):  # noqa: D102
         parser.add_argument(
-            '--package-whitelist', nargs='*', metavar='PKG_NAME',
+            '--packages-select', nargs='*', metavar='PKG_NAME',
             help='Only process a subset of packages')
         parser.add_argument(
-            '--package-blacklist', nargs='*', metavar='PKG_NAME',
+            '--packages-skip', nargs='*', metavar='PKG_NAME',
             help='Skip a set of packages')
 
         parser.add_argument(
-            '--package-whitelist-regex', nargs='*', metavar='PATTERN',
+            '--packages-select-regex', nargs='*', metavar='PATTERN',
             help='Only process a subset of packages where any of the patterns '
                  'match the package name')
         parser.add_argument(
-            '--package-blacklist-regex', nargs='*', metavar='PATTERN',
+            '--packages-skip-regex', nargs='*', metavar='PATTERN',
             help='Skip a set of packages where any of the patterns match the '
                  'package name')
 
     def check_parameters(self, args, pkg_names):  # noqa: D102
         # warn about ignored arguments
-        for pkg_name in args.package_whitelist or []:
+        for pkg_name in args.packages_select or []:
             if pkg_name not in pkg_names:
                 logger.warn(
                     "ignoring unknown package '{pkg_name}' in "
-                    '--package-whitelist'.format_map(locals()))
+                    '--packages-select'.format_map(locals()))
 
-        for pkg_name in args.package_blacklist or []:
+        for pkg_name in args.packages_skip or []:
             if pkg_name not in pkg_names:
                 logger.warn(
                     "ignoring unknown package '{pkg_name}' in "
-                    '--package-blacklist'.format_map(locals()))
+                    '--packages-skip'.format_map(locals()))
 
-        for pattern in list(args.package_whitelist_regex or []):
+        for pattern in list(args.packages_select_regex or []):
             # check patterns and remove invalid ones
             try:
                 re.compile(pattern)
             except Exception as e:
                 logger.warn(
-                    "the --package-whitelist-regex '{pattern}' failed to "
+                    "the --packages-select-regex '{pattern}' failed to "
                     'compile: {e}'.format_map(locals()))
-                args.package_whitelist_regex.remove(pattern)
+                args.packages_select_regex.remove(pattern)
                 continue
 
             if not any(re.match(pattern, pkg_name) for pkg_name in pkg_names):
                 logger.warn(
-                    "the --package-whitelist-regex '{pattern}' doesn't match "
+                    "the --packages-select-regex '{pattern}' doesn't match "
                     'any of the package names'.format_map(locals()))
 
-        for pattern in list(args.package_blacklist_regex or []):
+        for pattern in list(args.packages_skip_regex or []):
             # check patterns and remove invalid ones
             try:
                 re.compile(pattern)
             except Exception as e:
                 logger.warn(
-                    "the --package-blacklist-regex '{pattern}' failed to "
+                    "the --packages-skip-regex '{pattern}' failed to "
                     'compile: {e}'.format_map(locals()))
-                args.package_blacklist_regex.remove(pattern)
+                args.packages_skip_regex.remove(pattern)
                 continue
 
             if not any(re.match(pattern, pkg_name) for pkg_name in pkg_names):
                 logger.warn(
-                    "the --package-blacklist-regex '{pattern}' doesn't match "
-                    'any of the package names'.format_map(locals()))
+                    "the --packages-skip-regex '{pattern}' doesn't match any "
+                    'of the package names'.format_map(locals()))
 
     def select_packages(self, args, decorators):  # noqa: D102
         for decorator in decorators:
@@ -90,10 +88,10 @@ class WhitelistBlacklistPackageSelectionExtension(
             pkg = decorator.descriptor
 
             if (
-                pkg.name in (args.package_blacklist or []) or
+                pkg.name in (args.packages_skip or []) or
                 any(
                     re.match(pattern, pkg.name)
-                    for pattern in (args.package_blacklist_regex or [])
+                    for pattern in (args.packages_skip_regex or [])
                 )
             ):
                 logger.info(
@@ -102,17 +100,17 @@ class WhitelistBlacklistPackageSelectionExtension(
                 decorator.selected = False
 
             elif (
-                args.package_whitelist is not None or
-                args.package_whitelist_regex is not None
+                args.packages_select is not None or
+                args.packages_select_regex is not None
             ):
                 if (
-                    pkg.name not in (args.package_whitelist or []) and
+                    pkg.name not in (args.packages_select or []) and
                     not any(
                         re.match(pattern, pkg.name)
-                        for pattern in (args.package_whitelist_regex or [])
+                        for pattern in (args.packages_select_regex or [])
                     )
                 ):
                     logger.info(
-                        "Skipping not whitelisted package '{pkg.name}' in "
+                        "Skipping not selected package '{pkg.name}' in "
                         "'{pkg.path}'".format_map(locals()))
                     decorator.selected = False
